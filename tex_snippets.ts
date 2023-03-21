99e54d4c559af6ccc59e0da8028e327ac2f01560
Marco Yang <marco@unused-kidney.local>
2023-03-21 10:03:14 -0700

    vault backup: 2023-03-21 10:03:14

--- tex_snippets.ts
+++ tex_snippets.ts
@@ -270,6 +270,7 @@
     {trigger: "avg", replacement: "\\langle $0 \\rangle $1", options: "mA"},
     {trigger: "norm", replacement: "\\lvert $0 \\rvert $1", options: "mA", priority: 1},
     // {trigger: "mod", replacement: "|$0|$1", options: "mA"},
+    {trigger: "mod", replacement: "\\mod ", options: "m"},
     {trigger: "(", replacement: "(${VISUAL})", options: "mA"},
     {trigger: "[", replacement: "[${VISUAL}]", options: "mA"},
     {trigger: "{", replacement: "{${VISUAL}}", options: "mA"},
